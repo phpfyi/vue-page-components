cab9e1ea6b71a9b363b650785c1b87c2102f6aa1
Andrew Mc Cormack <webmaster@ddmseo.com>
2022-07-27 17:34:04 +0100

    add tag manager boot method

--- src/compositions/useGoogleTagManager.ts
+++ src/compositions/useGoogleTagManager.ts
@@ -1,13 +1,19 @@
 export function useGoogleTagManager() {
+    const bootGoogleTagManager = () => window.dataLayer = window.dataLayer || []
+
     const trackPage = (path: string, title: string): void => {
-        window.dataLayer = window.dataLayer || []
         window.dataLayer.push({
             event: 'PageView',
             page_path: path,
             page_title: title,
         })
     }
+    const pushData = (data: Record<string,unknown>): void => {
+        window.dataLayer.push(data)
+    }
     return {
+        bootGoogleTagManager,
         trackPage,
+        pushData
     }
 }
