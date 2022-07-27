083a10bea8e8d4069de87674259b7415eaa50c8d
Andrew Mc Cormack <webmaster@ddmseo.com>
2022-07-27 17:56:16 +0100

    add consent

---
+++ src/compositions/useConsent.ts
@@ -0,0 +1,39 @@
+import { reactive, ref } from 'vue'
+import * as Consent from '../types/Consent'
+import { useCookie } from '../compositions/useCookie'
+
+export const SETTINGS_COOKIE = 'phpfyi_settings'
+
+export function useConsent() {
+    const { getCookie, setCookie } = useCookie()
+
+    const consentLogged = ref(false)
+    let consent: Consent.Cookie = reactive({
+        preferences: false,
+        marketing: false,
+        statistics: false,
+    })
+    const getConsentCookie = (): Consent.Cookie | null => {
+        const cookie = String(getCookie(SETTINGS_COOKIE))
+
+        return cookie ? JSON.parse(cookie) : null
+    }
+    const setConsentCookie = (consent: Consent.Cookie | null) => {
+        setCookie(SETTINGS_COOKIE, JSON.stringify(consent))
+    }
+    const bootConsent = () => {
+        const cookie: Consent.Cookie | null = getConsentCookie()
+
+        if (cookie) {
+            consent = reactive(cookie)
+            consentLogged.value = true
+        }
+    }
+    return {
+        consent,
+        consentLogged,
+        bootConsent,
+        getConsentCookie,
+        setConsentCookie,
+    }
+}
---
+++ src/types/Consent.ts
@@ -0,0 +1,5 @@
+export interface Cookie {
+    preferences: boolean
+    marketing: boolean
+    statistics: boolean
+}
