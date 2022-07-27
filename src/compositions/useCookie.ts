1cc097a60dd5738688b97d25c5ff6cca927872ce
Andrew Mc Cormack <webmaster@ddmseo.com>
2022-07-27 01:12:44 +0100

    add useCookie composition

---
+++ src/compositions/useCookie.ts
@@ -0,0 +1,29 @@
+export function useCookie() {
+    const setCookie = (name: string, value: string, days = 30): void => {
+        let expires = ''
+        if (days) {
+            const date = new Date()
+            date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000)
+            expires = '; expires=' + date.toUTCString()
+        }
+        document.cookie = name + '=' + (value || '') + expires + '; path=/'
+    }
+    const getCookie = (name: string): null | string => {
+        const nameEQ = `${name}=`
+        const cookies = document.cookie.split(';')
+        for (let i = 0; i < cookies.length; i++) {
+            let cookie = cookies[i]
+            while (cookie.charAt(0) == ' ') cookie = cookie.substring(1, cookie.length)
+            if (cookie.indexOf(nameEQ) == 0) return cookie.substring(nameEQ.length, cookie.length)
+        }
+        return null
+    }
+    const deleteCookie = (name: string): void => {
+        document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;'
+    }
+    return {
+        setCookie,
+        getCookie,
+        deleteCookie,
+    }
+}
