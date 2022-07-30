d5b85c186533131003886046f3e9c58a6e98897a
Andrew Mc Cormack <webmaster@ddmseo.com>
2022-07-30 15:43:26 +0100

    add component types

---
+++ src/@types/component.d.ts
@@ -0,0 +1,26 @@
+import { VueConstructor } from "vue";
+
+export const PerspectivePlane: VueConstructor;
+export const PerspectiveWrapper: VueConstructor;
+export const SlantWrapper: VueConstructor;
+export const TransitionFade: VueConstructor;
+
+export const BaseHeading: VueConstructor;
+export const BaseText: VueConstructor;
+
+export const CheckboxInput: VueConstructor;
+export const EmailInput: VueConstructor;
+export const FormBase: VueConstructor
+export const FormMessages: VueConstructor
+export const InputLabel: VueConstructor
+export const PhoneNumberInput: VueConstructor
+export const SelectInput: VueConstructor
+export const SubmitInput: VueConstructor
+export const TextareaInput: VueConstructor
+export const TextInput: VueConstructor
+
+export const BaseImage: VueConstructor
+export const BaseIntersectionImage: VueConstructor
+
+export const BaseExternalLink: VueConstructor
+export const BaseLink: VueConstructor
