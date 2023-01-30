f28d75f5a40d6f8b75359b223a5b03ae3c8c3775
Andrew Mc Cormack <andrewmccormack@live.co.uk>
2023-01-30 20:28:31 +0000

    fix missing Link type export

--- lib/types.ts
+++ lib/types.ts
@@ -3,3 +3,4 @@ export * as Consent from '../src/types/Consent';
 export * as Element from '../src/types/Element';
 export * as Environment from '../src/types/Environment';
 export * as Form from '../src/types/Form';
+export * as Link from '../src/types/Link';
